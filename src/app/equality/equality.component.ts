e0200a4301e2cc05535c9232c4075dbfb2040023
Mehmet Zeki Kara <46008905+KaraZeki@users.noreply.github.com>
2022-07-03 23:47:27 +0300

    Math Game

---
+++ src/app/equality/equality.component.ts
@@ -0,0 +1,65 @@
+import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup } from '@angular/forms';
+import { delay, filter, scan } from 'rxjs/internal/operators';
+import { EqualityValidators } from '../equality-validators';
+
+@Component({
+  selector: 'app-equality',
+  templateUrl: './equality.component.html',
+  styleUrls: ['./equality.component.css']
+})
+export class EqualityComponent implements OnInit {
+
+  mathForm = new FormGroup({
+    firstNumber: new FormControl(this.generateNumber()),
+    secondNumber: new FormControl(this.generateNumber()),
+    answer: new FormControl('')
+
+  },
+    [
+      EqualityValidators.addition('answer','firstNumber','secondNumber')
+    ]
+  );
+
+  get firstNumber() {
+    return this.mathForm.value.firstNumber;;
+  }
+  get secondNumber() {
+    return this.mathForm.value.secondNumber;;
+  }
+
+
+  seconds:number=0;
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+    this.mathForm.statusChanges.pipe(
+      filter(value=>value=='VALID'),
+      delay(800),
+      scan( //172 . Vid
+        acc=>{
+         return {
+          numberSolved:acc.numberSolved+1,
+          startTime:acc.startTime
+         };
+        },
+        {numberSolved:0,startTime:new Date()}
+      )
+    )
+    .subscribe(({numberSolved,startTime})=>{
+      this.seconds=(new Date().getTime()-startTime.getTime())/numberSolved/1000
+      this.mathForm.setValue({
+        firstNumber:this.generateNumber(),
+        secondNumber:this.generateNumber(),
+        answer:''
+      })
+    })
+  }
+
+  generateNumber() {
+    return Math.floor(Math.random() * 10)
+  }
+
+}
